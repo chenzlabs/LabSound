59d433db50e66902afcef0e1418a75ba71757d86
avaer <a@modules.io>
2018-10-14 08:42:01 -0400

    Add magicleap-js/hack-toolchain.js hack to build-ml.sh

--- labsound/build-ml.sh
+++ labsound/build-ml.sh
@@ -4,6 +4,16 @@
 # npm install native-video-deps
 # popd
 
+if [ ! -d magicleap-js ]; then
+  git clone https://github.com/webmixedreality/magicleap-js
+else
+  pushd magicleap-js
+  git pull --rebase
+  popd
+fi
+
+./magicleap-js/hack-toolchain.js
+
 cmake -D CMAKE_TOOLCHAIN_FILE=.\\toolchain -D LUMIN=1 .
 # make clean
 make -j4
