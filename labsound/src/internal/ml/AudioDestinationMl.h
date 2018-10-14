a24782080d1956b4c762955009d8fb5c2cd55398
avaer <a@modules.io>
2018-10-14 07:39:02 -0400

    Comment out dead outputCallback

--- labsound/src/internal/ml/AudioDestinationMl.h
+++ labsound/src/internal/ml/AudioDestinationMl.h
@@ -59,7 +59,7 @@ public:
     std::vector<float> inputBuffer;
 };
 
-int outputCallback(void *outputBuffer, void *inputBuffer, unsigned int nBufferFrames, double streamTime, RtAudioStreamStatus status, void *userData ); 
+// int outputCallback(void *outputBuffer, void *inputBuffer, unsigned int nBufferFrames, double streamTime, RtAudioStreamStatus status, void *userData ); 
 
 } // namespace lab
 
